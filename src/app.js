9f429bd1ce0719b59992e7fd981fda40743a3802
Jaime B <j.barajas51801@gmail.com>
2025-07-11 14:18:56 -0500

    updating text to have .com at end

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ function combos(array1 ,array2, array3)
       for(const value3 of array3)
       {
         
-        combinations.set(iterator,`${value1} ${value2} ${value3}`);
+        combinations.set(iterator,`${value1} ${value2} ${value3}.com`);
         iterator++;
       }
     }
@@ -39,6 +39,6 @@ window.onload = function()
 {
   const sentences = combos(pronoun, adj, noun); // Store return value
   const para = document.getElementById('combos');
-  para.textContent = sentences.join('\n');
+  para.innerHTML = sentences.join('<br>');
 
 };
